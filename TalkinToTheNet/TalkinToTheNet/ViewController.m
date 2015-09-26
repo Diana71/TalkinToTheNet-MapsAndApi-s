3e05b28e57d6b5542626b058c90e86fda12e0423
Diana <diana_elezaj@hotmail.com>
2015-09-25 21:29:03 -0400

    Homework

--- TalkinToTheNet/TalkinToTheNet/ViewController.m
+++ TalkinToTheNet/TalkinToTheNet/ViewController.m
@@ -7,21 +7,121 @@
 //
 
 #import "ViewController.h"
+@import GoogleMaps;
 
 @interface ViewController ()
 
 @end
 
-@implementation ViewController
+@implementation ViewController {
+    CLLocationManager *locationManager;
+    
+     GMSMapView *mapView_;
+    BOOL firstLocationUpdate_;
+}
 
 - (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    //instantiate the CLLocationManager object
+    locationManager = [[CLLocationManager alloc] init];
+    
+    locationManager.delegate = self;
+    locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+    
+    [locationManager requestWhenInUseAuthorization];
+    
+    [locationManager startUpdatingLocation];
+    
+    mapView_.myLocationEnabled = YES;
+    
+    
+    // Create a GMSCameraPosition that tells the map to display the
+    // coordinate -33.86,151.20 at zoom level 6.
+    GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:40.7141667
+                                                            longitude:-74.0063889
+                                                                 zoom:6];
+    mapView_ = [GMSMapView mapWithFrame:CGRectZero camera:camera];
+    mapView_.settings.compassButton = YES;
+    mapView_.myLocationEnabled = YES;
+    mapView_.settings.myLocationButton = YES;
+    self.view = mapView_;
+    
+    // Creates a marker in the center of the map.
+    GMSMarker *marker = [[GMSMarker alloc] init];
+    marker.position = CLLocationCoordinate2DMake(40.7141667, -74.0063889);
+    marker.title = @"Sydney";
+    marker.snippet = @"Australia";
+    marker.map = mapView_;
+    
+    // Listen to the myLocation property of GMSMapView.
+    [mapView_ addObserver:self
+               forKeyPath:@"myLocation"
+                  options:NSKeyValueObservingOptionNew
+                  context:NULL];
+    
+    self.view = mapView_;
+    
+    // Ask for My Location data after the map has already been added to the UI.
+    dispatch_async(dispatch_get_main_queue(), ^{
+        mapView_.myLocationEnabled = YES;
+//        NSLog(@"my loc %@",mapView_.myLocation);
+//        NSLog(@"pos %c",marker.position);
+    });
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSString *,id> *)change context:(void *)context {
+    if ([keyPath isEqualToString:@"myLocation"]) {
+        NSLog(@"MIKEEEEEE lat %f", mapView_.myLocation.coordinate.latitude);
+        NSLog(@"MIKEEEEEE long %f", mapView_.myLocation.coordinate.longitude);
+        NSLog(@"keyPath %@", keyPath);
+        // Creates a marker in the center of the map.
+//            GMSMarker *marker = [[GMSMarker alloc] init];
+//            marker.position = CLLocationCoordinate2DMake(mapView_.myLocation.coordinate.latitude, mapView_.myLocation.coordinate.longitude);
+//        
+//            marker.title = @"New York";
+//            marker.snippet = @"NY";
+//            marker.map = mapView_;
+        
+        
+    } else {
+        
+        NSLog(@"Else statement");
+        [super observeValueForKeyPath:keyPath ofObject:object change:change context:context];
+    }
 }
+
+
+#pragma mark - CLLocationManagerDelegate
+
+- (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error
+{
+    NSLog(@"didFailWithError: %@", error);
+    UIAlertView *errorAlert = [[UIAlertView alloc]
+                               initWithTitle:@"Error" message:@"Failed to Get Your Location" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+    [errorAlert show];
+}
+
+//- (void)locationManager:(CLLocationManager *)manager didUpdateToLocation:(CLLocation *)newLocation fromLocation:(CLLocation *)oldLocation
+//{
+//    NSLog(@"didUpdateToLocation: %@", newLocation);
+//    CLLocation *currentLocation = newLocation;
+//    
+//    if (currentLocation != nil) {
+//        NSLog ( @"longitude %.8f", currentLocation.coordinate.longitude);
+//        NSLog ( @"latitude %.8f", currentLocation.coordinate.latitude);
+//    }
+//}
+
+
+
+
+
+- (BOOL)didTapMyLocationButtonForMapView:(GMSMapView *)mapView {
+    return YES;
+}
+
+
+
+
 
 @end
